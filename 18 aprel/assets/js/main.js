a8c23175f582784e644a7fbf03ad78a1bbb1e606
Konul Yusifova <127186343+konulyusifova@users.noreply.github.com>
2023-04-19 13:51:47 +0400

    Add files via upload

--- 18 aprel/assets/js/main.js
+++ 18 aprel/assets/js/main.js
@@ -94,31 +94,112 @@
 
 
 
-// Verilmis string deyerinde verilmis char-in hansi index-de yerlesdiyini tapibb qaytaran funksiyani qurun
-function findIndex(str,char){
-    for (let i = 0; i< str.length; i++) {
-        if(str[i]==char){
-            return i;
-        }
-        else{
+// Verilmis string deyerinde verilmis char-in hansi index-de yerlesdiyini tapibb qaytaran funksiyani qurun.
+
+// function findIndex(str,char){
+//     for (let i = 0; i< str.length; i++) {
+//         if(str[i]==char){
+//             return i;
+//         }
+//         else{
+//         }
+//     }
+//     return -1;
+// }
+// console.log(findIndex(prompt(),prompt()));
+
+
+
+
+
+
+// Ededlerden ibaret array verilib. Arrayin en kicik elementini geri qaytaran funksiya yazin.
+
+// function findMin(n) {
+//     min = n[0];
+//     for (i = 1; i < n.length; i++) {
+//         if (n[i] < min) {
+//             min = n[i];
+//         }
+//     }
+//     return min;
+// }
+// console.log(findMin([12, 23, 34, 45, 56, 67, 78, 233]));
+
+
+
+
+
+
+// Ededlerden ibaret array verilib. Arrayin en boyuk elementini geri qaytaran funksiya yazin.
+
+// function findMax(n) {
+//     max= n[0];
+//     for (i = 1; i < n.length; i++) {
+//         if (n[i] > max) {
+//             max = n[i];
+//         }
+//     }
+//     return max;
+// }
+// console.log(findMax([12, 23, 34, 45, 56, 67, 78, 233]));
+
+
+
+
+// Cut yerde dayanan ededlerin cemini tap:
+
+// function findEven(num){
+//     var sum=0;
+//     for(i=0; i<num.length; i+=2){
+//         sum+=num[i];
+//     }
+//     return sum;
+// }
+// console.log(findEven([12,23,34,2354,344,566]));
+
+
+
+
+
+// Tek yerde dayanan en balaca ededle, cut yerde dayanan en boyuk ededi cemle:
+
+// function sumNumbers(num) {
+//     var sum = 0;
+//     var max=num[0];
+//     var min=num[0]
+//     for (i = 0; i < num.length; i++) {
+//         if (num[i]%2==0) {
+//             if(num[i]>max){
+//                 max = num[i];
+//             }
+//         }
+//         else {
+//             if(num[i]<min){
+//                 min = num[i];
+//             }
+//         }
+
+
+
+
+
+//         sum = max + min;
+//     }
+//     return sum;
+// }
+// console.log(sumNumbers([12, 23, 34, 24, 344, 566, 1, 2446, 1244]));
+
+
+
+
+// Verilen ededin 2-nin quvveti olub olmadigini yoxlayin, eger 2-nin quvvetidirse ekrana yazan funksiya
+
+function twoPower(num) {
+    for(i=0; i<num.length; i++){
+        if(num%2===0 && num/2===1){
+           console.log(num);
         }
     }
-    return -1;
 }
-console.log(findIndex(prompt(),prompt()));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+twoPower(64);
